9ea4cf38d2aa70f1c2ccabaf784b47aca2ae8d3f
jwd2a <jdavis1002@gmail.com>
2013-03-21 11:47:36 -0400

    Footer styled, GMaps added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -40,8 +40,30 @@
 			$("html,body").animate({ scrollTop: $("#welcome").offset().top}, 250);
 		});
 		
+		
+		var e = {
+			zoom: 16,
+			center: new google.maps.LatLng(27.947339, -82.460533),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		
+		var map=new google.maps.Map(document.getElementById("map"),e);
+		var markerOpts = {
+			position: new google.maps.LatLng(27.947339, -82.460533),
+		}
+		var marker = new google.maps.Marker(markerOpts);
+		marker.setMap(map);
+		
 	});
 
 function changeVentureBlurb(number){
 	$("#displayedVentureBlurb").html($("[index='blurb"+number+"']").html());
 }
+
+
+
+
+
+
+
+
